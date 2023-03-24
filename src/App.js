396ec8efb7d142d558da9fd887086121e3561af3
Rick Navarro <73374566+NavarroErk@users.noreply.github.com>
2023-03-24 00:04:18 -0700

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          THIS IS APP.js
         </p>
         <a
           className="App-link"
